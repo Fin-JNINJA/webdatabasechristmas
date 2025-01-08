520c184f5c56b027d61fa2a611640f098a460fc8
Fin-JNINJA <finlay.jolley@gmail.com>
2025-01-08 21:35:35 +0000

    website

---
+++ website/website/index.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const ejs = require('ejs');
+const util = require('util');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+
+
+/**
+ * The following constants with your MySQL connection properties
+ * You should only need to change the password
+ */
+
+const PORT = 8000;
+const DB_HOST = 'localhost';
+const DB_USER = 'root';
+const DB_PASSWORD = 'password';
+const DB_NAME = 'coursework';
+const DB_PORT = 3306;
+
+/**
+ * DO NOT CHANGE ANYTHING BELOW THIS LINE UP TO THE NEXT COMMENT
+ */
+var connection = mysql.createConnection({
+	host: DB_HOST,
+	user: DB_USER,
+	password: DB_PASSWORD,
+	database: DB_NAME,
+	port: DB_PORT
+});
+
+connection.query = util.promisify(connection.query).bind(connection);
+
+connection.connect(function (err) {
+	if (err) {
+		console.error('error connecting: ' + err.stack);
+		console.log('Please make sure you have updated the password in the index.js file. Also, ensure you have run db_setup.sql to create the database and tables.');
+		return;
+	}
+	console.log('Connected to the Database');
+});
+
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+/**
+ * YOU CAN CHANGE THE CODE BELOW THIS LINE
+ */
+
+// Add your code here
+
+app.get('/', async (req, res) => {
+	res.send('Hello World');
+});
+
+
+
+
+
+
+
+
+/**
+ * DON'T CHANGE ANYTHING BELOW THIS LINE
+ */
+
+app.listen(PORT, () => {
+
+	console.log(`Server is running on port http://localhost:${PORT}`);
+
+});
+
+
+
+exports.app = app;
